292d246ab2d8d966238dece8f54fe9a654872ffa
Vitor Silva <vitorabsilva10@gmail.com>
2021-02-25 00:09:51 -0300

    Correct enxt-env.d.ts file

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-=======
-/// <reference types="next" />
-/// <reference types="next/types/global" />
->>>>>>> a31ab4034d689fd00d3585c232e820e3bdbdbc66
